31f0b3d9895fa679263f6cb1c2cfd8308b6a9e67
dbeattie <darren@fettle.ca>
2019-12-17 20:52:37 +0000

    changed some colors and background images
    created character countdown feature using jQuery

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $(".new-tweet textarea").on('keyup', function() {    
+    const remainingChar = 140 - $(this).val().length;
+    const charCounter = $(this).closest(".new-tweet").find(".counter");
+    if (remainingChar < 0){
+      charCounter.addClass("char-limit");
+    } else {
+      charCounter.removeClass("char-limit");
+    }    
+    charCounter.text(remainingChar);
+  });
+});
+
+
